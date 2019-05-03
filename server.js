ab49bd8b9619b519730828b2cdc0846f0a37aa14
Vivek Manjunath <vivekngd@gmail.com>
2019-05-02 23:37:24 -0400

    tcl package json updates

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use(routes);
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('build'));
+    app.use(express.static('/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
     })   
